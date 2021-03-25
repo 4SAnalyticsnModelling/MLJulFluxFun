45027db6f7bf6d6c5345f08820931fab88cd8d73
Symon <59005260+4SAnalyticsnModelling@users.noreply.github.com>
2021-03-24 23:42:37 -0600

    Add files via upload

---
+++ src/MLJulFluxFun.jl
@@ -0,0 +1,6 @@
+# This script contains functions that are predefined to be used in other scripts.
+# Created by and © 4S Analytics & Modelling Ltd.
+module
+include("flux_model_perform_ttgiven_save_trained_mod.jl");
+include("flux_model_perform_save_trained_mod.jl");
+end
