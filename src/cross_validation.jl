5f8215811f002a45826f00c643356b8c08da895c
Symon <59005260+4SAnalyticsnModelling@users.noreply.github.com>
2021-06-02 09:16:31 -0600

    Update cross_validation.jl

--- src/cross_validation.jl
+++ src/cross_validation.jl
@@ -1,31 +1,38 @@
 # Different cross-validation techniques
 using Random;
 # Holdout
-function Holdout(ids :: Any, train_frac :: Float64, shuffle_id :: Bool = false)
+function Holdout(ids :: T, train_frac :: Float64, shuffle_id :: Bool = false, nsample :: Int64 = 1) where {T <: Union{UnitRange{Int64}, StepRange{Int64, Int64}, Base.OneTo{Int64}, Vector{Int64}}}
+    train_test_pairs = []
     ids_mat = collect(ids)
-    if shuffle_id
-        Random.shuffle!(ids_mat)
+    for i in 1:nsample
+        if shuffle_id
+            Random.shuffle!(ids_mat)
+        end
+        break_point = trunc(Int64, round(size(ids_mat)[1] * train_frac))
+        train_test = (ids_mat[1:break_point], ids_mat[(break_point + 1):size(ids_mat)[1]])
+        train_test_pairs = vcat(train_test_pairs, train_test)
     end
-    break_point = trunc(Int64, round(size(ids_mat)[1] * train_frac))
-    train_test_pairs = (ids_mat[1:break_point], ids_mat[(break_point + 1):size(ids_mat)[1]])
     return train_test_pairs
+end
 end
 # KFold
-function KFold(ids :: Any, k :: Int64, shuffle_id ::Bool = false)
+function KFold(ids :: T, k :: Int64, shuffle_id ::Bool = false, nsample :: Int64 = 1) where {T <: Union{UnitRange{Int64}, StepRange{Int64, Int64}, Base.OneTo{Int64}, Vector{Int64}}}
     ids_mat = collect(ids)
-    if shuffle_id
-        Random.shuffle!(ids_mat)
-    end
-    paired_mat = [ids_mat vcat(repeat(collect(1:k), inner = div(size(ids_mat)[1], k)), collect(1:k)[1:rem(size(ids_mat)[1], k)])]
     train_test_pairs = []
-    for i in 1:k
-        train_test = (paired_mat[paired_mat[:, 2] .!= i, 1], paired_mat[paired_mat[:, 2] .== i, 1])
-        push!(train_test_pairs, train_test)
+    for i in 1:nsample
+        if shuffle_id
+            Random.shuffle!(ids_mat)
+        end
+        paired_mat = [ids_mat vcat(repeat(collect(1:k), inner = div(size(ids_mat)[1], k)), collect(1:k)[1:rem(size(ids_mat)[1], k)])]
+        for i in 1:k
+            train_test = (paired_mat[paired_mat[:, 2] .!= i, 1], paired_mat[paired_mat[:, 2] .== i, 1])
+            push!(train_test_pairs, train_test)
+        end
     end
     return train_test_pairs
 end
-# Grouped KFold      
-function GroupedKFold(ids :: Any, group_list :: Any)
+# Grouped KFold
+function GroupedKFold(ids :: T, group_list :: S) where {T <: Union{UnitRange{Int64}, StepRange{Int64, Int64}, Base.OneTo{Int64}, Vector{Int64}}, S <: Union{Vector{Union{String, Missing, Int64}}}}
     paired_mat = [collect(ids) collect(group_list)]
     train_test_pairs = []
     for groups in unique(group_list)
