2cb1dea7429cc11c01ec479a49f102b376a94558
Symon <59005260+4SAnalyticsnModelling@users.noreply.github.com>
2021-06-02 10:58:03 -0600

    Update utility_functions.jl

--- src/utility_functions.jl
+++ src/utility_functions.jl
@@ -1,20 +1,21 @@
 # Utility and wrapper functions to be used in training and evaluating models
 using Flux;
-using Flux.Zygote;
 # Root mean squared error
-function rmse(obs :: Vector, pred :: Vector)
-    return sqrt(sum((obs .- pred).^2)/length(obs))
+function rmse(obs :: T, pred :: T) where {T <: Union{UnitRange{Int64}, StepRange{Int64, Int64}, Base.OneTo{Int64}, Vector{Int64}, UnitRange{Float64}, StepRange{Float64, Float64}, Vector{Float64}}}
+    return sqrt(sum((collect(obs) .- collect(pred)).^2)/length(collect(obs)))
 end
+
 # Loss function for Flux models
 function loss(flux_model, x, y)
-    return sqrt(sum((y .- vec(flux_model(x))).^2)/length(y))
+    return sqrt(sum((vec(y) .- vec(flux_model(x))).^2)/length(vec(y)))
 end
 # Custom training function for Flux models
-function my_custom_train!(flux_model, loss, ps, data, optimizer)
-    ps = Zygote.Params(ps)
+function my_custom_train!(flux_model, loss, data, optimizer)
+    ps = Flux.params(flux_model)
     for d in data
-        train_loss, back = Zygote.pullback(() -> loss(flux_model, d...), ps)
-        gs = back(one(train_loss))
-        Flux.Optimise.update!(optimizer, ps, gs)
+      gs = Flux.gradient(ps) do
+        training_loss = loss(flux_model, d...)
+      end
+      Flux.update!(optimizer, ps, gs)
     end
-end
+ end
