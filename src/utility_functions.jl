4a405d81339079e8ab3d2a0bda7f9b91f6863f59
Symon <59005260+4SAnalyticsnModelling@users.noreply.github.com>
2021-06-02 11:23:03 -0600

    Update utility_functions.jl

--- src/utility_functions.jl
+++ src/utility_functions.jl
@@ -1,7 +1,7 @@
 # Utility and wrapper functions to be used in training and evaluating models
 using Flux;
 # Root mean squared error
-function rmse(obs :: T, pred :: T) where {T <: Union{UnitRange{Int64}, StepRange{Int64, Int64}, Base.OneTo{Int64}, Vector{Int64}, UnitRange{Float64}, StepRange{Float64, Float64}, Vector{Float64}}}
+function rmse(obs :: T, pred :: S) where {T <: Union{UnitRange{Int64}, StepRange{Int64, Int64}, Base.OneTo{Int64}, Vector{Int64}, UnitRange{Float64}, StepRange{Float64, Float64}, Vector{Float64}, Vector{Float32}, Vector{Number}}, S <: Union{UnitRange{Int64}, StepRange{Int64, Int64}, Base.OneTo{Int64}, Vector{Int64}, UnitRange{Float64}, StepRange{Float64, Float64}, Vector{Float64}, Vector{Float32}, Vector{Number}}}
     return sqrt(sum((collect(obs) .- collect(pred)).^2)/length(collect(obs)))
 end
 
