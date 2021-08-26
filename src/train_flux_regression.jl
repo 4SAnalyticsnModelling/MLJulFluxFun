5df71f9aa9be6d1892835798635dc3a5d287bbc0
Symon 4S <mezbahu@gmail.com>
2021-08-26 14:31:24 -0600

    change loss functions

--- src/train_flux_regression.jl
+++ src/train_flux_regression.jl
@@ -32,14 +32,14 @@ function flux_mod_eval(flux_model,
         data = Flux.Data.DataLoader((x_train, y_train), shuffle = true, batchsize = nobs_per_batch)
         for j in 1:n_epochs
             my_custom_train!(flux_model, loss, data, optimizer)
-            train_loss = loss(flux_model, x_train, y_train)
+            train_loss = loss(flux_model, loss_init, x_train, y_train)
             ps = Flux.params(flux_model)
             println("epoch = " * string(j) * " training_loss = " * string(train_loss))
             if pullback
                 flux_model1 = flux_model
                 Flux.loadparams!(flux_model1, ps)
                 my_custom_train!(flux_model1, loss, data, optimizer)
-                train_loss_1 = loss(flux_model1, x_train, y_train)
+                train_loss_1 = loss(flux_model1, loss_init, x_train, y_train)
                 ps1 = Flux.params(flux_model1)
                 if train_loss < train_loss_1
                     train_loss_record = []
@@ -47,7 +47,7 @@ function flux_mod_eval(flux_model,
                     Flux.loadparams!(flux_model2, ps1)
                     for l in 1:(lcheck - 1)
                         my_custom_train!(flux_model2, loss, data, optimizer)
-                        train_loss_2 = loss(flux_model2, x_train, y_train)
+                        train_loss_2 = loss(flux_model2, loss_init, x_train, y_train)
                         push!(train_loss_record, train_loss_2)
                     end
                     if sum(train_loss .< train_loss_record) == (lcheck - 1)
@@ -82,14 +82,14 @@ function flux_mod_eval(flux_model,
             data = Flux.Data.DataLoader((x_train, y_train), shuffle = true, batchsize = nobs_per_batch)
             for j in 1:n_epochs
                 my_custom_train!(flux_model1, loss, data, optimizer)
-                valid_loss = loss(flux_model1, x_test, y_test)
+                valid_loss = loss(flux_model1, loss_init, x_test, y_test)
                 println("epoch = " * string(j) * " validation_loss = " * string(valid_loss))
                 ps1 = Flux.params(flux_model1)
                 if pullback
                     flux_model2 = flux_model1
                     Flux.loadparams!(flux_model2, ps1)
                     my_custom_train!(flux_model2, loss, data, optimizer)
-                    valid_loss_1 = loss(flux_model2, x_test, y_test)
+                    valid_loss_1 = loss(flux_model2, loss_init, x_test, y_test)
                     ps2 = Flux.params(flux_model2)
                     if valid_loss < valid_loss_1
                         valid_loss_record = []
@@ -97,7 +97,7 @@ function flux_mod_eval(flux_model,
                         Flux.loadparams!(flux_model3, ps2)
                         for l in 1:(lcheck - 1)
                             my_custom_train!(flux_model3, loss, data, optimizer)
-                            valid_loss_2 = loss(flux_model3, x_test, y_test)
+                            valid_loss_2 = loss(flux_model3, loss_init, x_test, y_test)
                             push!(valid_loss_record, valid_loss_2)
                         end
                         if sum(valid_loss .< valid_loss_record) == (lcheck - 1)
