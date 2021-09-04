34c65e39254f903e7f6a7d6c1d29eb8847b49a6e
Symon 4S <mezbahu@gmail.com>
2021-09-04 03:24:54 -0600

    revise scaler functions

--- src/utility_functions.jl
+++ src/utility_functions.jl
@@ -12,7 +12,7 @@ function my_custom_train!(flux_model, ps, loss, loss_init, data, optimizer)
     for d in data
         train_loss, back = Zygote.pullback(() -> loss(flux_model, loss_init, d...), ps)
         gs = back(one(train_loss))
-        any(isnan.(gs[ps]))
+        any(isnan.(back(1.)[ps]))
         Flux.update!(optimizer, ps, gs)
     end
  end
