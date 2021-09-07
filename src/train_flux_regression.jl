13c10d2bebba823832de5e2bcaba88c119cc940d
Symon 4S <mezbahu@gmail.com>
2021-09-07 15:52:15 -0600

    add NaN skip

--- src/train_flux_regression.jl
+++ src/train_flux_regression.jl
@@ -40,7 +40,9 @@ function flux_mod_eval(flux_model,
             y_train = y[train]
         end
         data = Flux.Data.DataLoader((x_train, y_train), shuffle = true, batchsize = nobs_per_batch)
-        for j in 1:n_epochs
+        n = n_epochs + 1
+        j = 0
+        while j < n
             my_custom_train!(flux_model, ps_init, oss, loss_init, data, optimizer)
             train_loss = loss(flux_model, loss_init, x_train, y_train)
             if isnan(train_loss) == false
@@ -56,10 +58,12 @@ function flux_mod_eval(flux_model,
                         train_loss_record = []
                         flux_model2 = flux_model
                         Flux.loadparams!(flux_model2, ps1)
-                        for l in 1:(lcheck - 1)
+                        l = 0
+                        while l < lcheck
                             my_custom_train!(flux_model2, ps1, loss, loss_init, data, optimizer)
                             train_loss_2 = loss(flux_model2, loss_init, x_train, y_train)
                             push!(train_loss_record, train_loss_2)
+                            l += 1
                         end
                         if sum(train_loss .< train_loss_record) == (lcheck - 1)
                             try
@@ -71,30 +75,31 @@ function flux_mod_eval(flux_model,
                         end
                     end
                 end
-                y_pred_train = vec(flux_model(x_train))
-                if isnothing(scaler) == false
-                    y_train = StatsBase.reconstruct(y_scaler, y_train)
-                    y_pred_train = StatsBase.reconstruct(y_scaler, y_pred_train)
-                end
-                r2_train = round((Statistics.cor(y_train, y_pred_train))^2, digits = r_squared_precision)
-                rmse_train = round(sqrt(Flux.Losses.mse(y_pred_train, y_train)), digits = rmse_precision)
-                weights = Flux.params(Flux.cpu(flux_model))
-                BSON.@save(save_trained_model_at * "/trained_model.bson", weights)
-                model_perform = [r2_train rmse_train]
-                model_perform_df = DataFrame(model_perform[1, :]', [:r_squared_train, :rmse_train])
-                CSV.write(save_trained_model_at * "/model_training_records.csv", model_perform_df)
             else
-                n_epochs += 1
+                n += 1
                 try
-                    Flux.stop()
+                    Flux.skip()
                 catch
                 finally
                 end
-                break
             end
+            j += 1
         end
+        y_pred_train = vec(flux_model(x_train))
+        if isnothing(scaler) == false
+            y_train = StatsBase.reconstruct(y_scaler, y_train)
+            y_pred_train = StatsBase.reconstruct(y_scaler, y_pred_train)
+        end
+        r2_train = round((Statistics.cor(y_train, y_pred_train))^2, digits = r_squared_precision)
+        rmse_train = round(sqrt(Flux.Losses.mse(y_pred_train, y_train)), digits = rmse_precision)
+        weights = Flux.params(Flux.cpu(flux_model))
+        BSON.@save(save_trained_model_at * "/trained_model.bson", weights)
+        model_perform = [r2_train rmse_train]
+        model_perform_df = DataFrame(model_perform[1, :]', [:r_squared_train, :rmse_train])
+        CSV.write(save_trained_model_at * "/model_training_records.csv", model_perform_df)
     else
-        for k in 1:size(cv_strategy)[1]
+        k = 0
+        while k < (1 + size(cv_strategy)[1])
             flux_model1 = flux_model
             Flux.loadparams!(flux_model1, ps_init)
             train, test = cv_strategy[k, ]
@@ -114,7 +119,9 @@ function flux_mod_eval(flux_model,
                 y_test = y[test]
             end
             data = Flux.Data.DataLoader((x_train, y_train), shuffle = true, batchsize = nobs_per_batch)
-            for j in 1:n_epochs
+            n = n_epochs + 1
+            j = 0
+            while j < n
                 my_custom_train!(flux_model1, ps_init, loss, loss_init, data, optimizer)
                 valid_loss = loss(flux_model1, loss_init, x_test, y_test)
                 if isnan(valid_loss) == false
@@ -130,10 +137,12 @@ function flux_mod_eval(flux_model,
                             valid_loss_record = []
                             flux_model3 = flux_model2
                             Flux.loadparams!(flux_model3, ps2)
-                            for l in 1:(lcheck - 1)
+                            l = 0
+                            while l < lcheck
                                 my_custom_train!(flux_model3, ps2, loss, loss_init, data, optimizer)
                                 valid_loss_2 = loss(flux_model3, loss_init, x_test, y_test)
                                 push!(valid_loss_record, valid_loss_2)
+                                l += 1
                             end
                             if sum(valid_loss .< valid_loss_record) == (lcheck - 1)
                                 try
@@ -145,41 +154,42 @@ function flux_mod_eval(flux_model,
                             end
                         end
                     end
-                    y_pred = vec(flux_model1(x_test))
-                    y_pred_train = vec(flux_model1(x_train))
-                    if isnothing(scaler) == false
-                        y_train = StatsBase.reconstruct(y_scaler, y_train)
-                        y_test = StatsBase.reconstruct(y_scaler, y_test)
-                        y_pred_train = StatsBase.reconstruct(y_scaler, y_pred_train)
-                        y_pred = StatsBase.reconstruct(y_scaler, y_pred)
-                    end
-                    r2_test = round((Statistics.cor(y_test, y_pred))^2, digits = r_squared_precision)
-                    rmse_test = round(sqrt(Flux.Losses.mse(y_pred, y_test)), digits = rmse_precision)
-                    r2_train = round((Statistics.cor(y_train, y_pred_train))^2, digits = r_squared_precision)
-                    rmse_train = round(sqrt(Flux.Losses.mse(y_pred_train, y_train)), digits = rmse_precision)
-                    weights = Flux.params(Flux.cpu(flux_model))
-                    BSON.@save(save_trained_model_at * "/trained_model.bson", weights)
-                    model_perform = [k r2_test r2_train rmse_test rmse_train]
-                    if k == 1
-                        CSV.write(save_trained_model_at * "/model_training_records.csv", DataFrame(model_perform, [:iter, :r_squared_test, :r_squared_train, :rmse_test, :rmse_train]))
-                    else
-                        CSV.write(save_trained_model_at * "/model_training_records.csv", DataFrame(model_perform, [:iter, :r_squared_test, :r_squared_train, :rmse_test, :rmse_train]), append = true)
-                    end
-                    model_perform_mat = vcat(model_perform_mat, model_perform)
-                    model_perform_mat = sortslices(model_perform_mat, dims = 1, by = x -> (x[4], x[5]), rev = false)
-                    model_perform_mat = sortslices(model_perform_mat, dims = 1, by = x -> (x[2], x[3]), rev = true)
-                    model_perform_mat = model_perform_mat[1, :]'
-                    model_perform_df = DataFrame(model_perform_mat, [:iter, :r_squared_test, :r_squared_train, :rmse_test, :rmse_train])
                 else
-                    n_epochs += 1
+                    n += 1
                     try
-                        Flux.stop()
+                        Flux.skip()
                     catch
                     finally
                     end
-                    break
                 end
+                j += 1
             end
+            y_pred = vec(flux_model1(x_test))
+            y_pred_train = vec(flux_model1(x_train))
+            if isnothing(scaler) == false
+                y_train = StatsBase.reconstruct(y_scaler, y_train)
+                y_test = StatsBase.reconstruct(y_scaler, y_test)
+                y_pred_train = StatsBase.reconstruct(y_scaler, y_pred_train)
+                y_pred = StatsBase.reconstruct(y_scaler, y_pred)
+            end
+            r2_test = round((Statistics.cor(y_test, y_pred))^2, digits = r_squared_precision)
+            rmse_test = round(sqrt(Flux.Losses.mse(y_pred, y_test)), digits = rmse_precision)
+            r2_train = round((Statistics.cor(y_train, y_pred_train))^2, digits = r_squared_precision)
+            rmse_train = round(sqrt(Flux.Losses.mse(y_pred_train, y_train)), digits = rmse_precision)
+            weights = Flux.params(Flux.cpu(flux_model))
+            BSON.@save(save_trained_model_at * "/trained_model.bson", weights)
+            model_perform = [k r2_test r2_train rmse_test rmse_train]
+            if k == 1
+                CSV.write(save_trained_model_at * "/model_training_records.csv", DataFrame(model_perform, [:iter, :r_squared_test, :r_squared_train, :rmse_test, :rmse_train]))
+            else
+                CSV.write(save_trained_model_at * "/model_training_records.csv", DataFrame(model_perform, [:iter, :r_squared_test, :r_squared_train, :rmse_test, :rmse_train]), append = true)
+            end
+            model_perform_mat = vcat(model_perform_mat, model_perform)
+            model_perform_mat = sortslices(model_perform_mat, dims = 1, by = x -> (x[4], x[5]), rev = false)
+            model_perform_mat = sortslices(model_perform_mat, dims = 1, by = x -> (x[2], x[3]), rev = true)
+            model_perform_mat = model_perform_mat[1, :]'
+            model_perform_df = DataFrame(model_perform_mat, [:iter, :r_squared_test, :r_squared_train, :rmse_test, :rmse_train])
+            k += 1
         end
     end
     return model_perform_df :: DataFrame
