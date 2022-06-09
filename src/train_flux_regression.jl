6cc38d31aa5c4fe1c348434509d1f03f2c096429
Symon <59005260+4SAnalyticsnModelling@users.noreply.github.com>
2022-06-09 11:10:27 -0600

    Add files via upload

--- src/train_flux_regression.jl
+++ src/train_flux_regression.jl
@@ -155,13 +155,16 @@ function flux_mod_stack(flux_model_builder :: Any,
     x :: DataFrame,
     y :: Vector,
     save_trained_model_at :: String,
+	cv_strategy_train :: Union{Holdout_, KFold_, GroupedKFold_},
     scaler_x :: Union{Nothing, max_min_scaler, standard_scaler},
+	meta_train_frac :: Float64,
     n_epochs :: Int64,
+	pullback :: Bool,
+	lcheck :: Int64,
     nobs_per_batch :: Int64,
 	save_trained_model :: Bool,
     loss_init,
     optimizer,
-	n_boot :: Int64,
 	nthreads_ :: Int64 = 1,
 	r_squared_precision :: Int64 = 3,
     rmse_precision :: Int64 = 2)
@@ -173,75 +176,164 @@ function flux_mod_stack(flux_model_builder :: Any,
 		    mkdir(save_trained_model_at * "/saved_ensembled_meta_Xscaler(s)")
 	    end
     end
-    model_perform = Array{Float64}(undef, 0, 3)
+    model_perform = Array{Float64}(undef, 0, 5)
 	x_meta = DataFrame()
 	y_meta = []
-	for k0 in Distributed.splitrange(1, n_boot, nthreads_)
+	cv_train = cross_validate(cv_strategy_train, eachindex(y))
+	for k0 in Distributed.splitrange(1, size(cv_train)[1], nthreads_)
 		t = Threads.@spawn begin
 			for k in k0
-		    	flux_model = flux_model_builder
-				# Perform bootstrap (bagging) training
-				train = [rand(eachindex(y)) for _ in 1:length(y)]
-				append!(x_meta, x[train, :])
+		    	# Perform bootstrap (bagging) training
+				flux_model = flux_model_builder
+				train, valid = cv_train[k, ]
+				if pullback == true
+					valid_loss_record = []
+					early_stop_flag = 0
+					model_dict = Dict()
+				end
 				if isnothing(scaler_x) == false
-		            x_scaler = fit_scaler(scaler_x, Matrix(x[train, :]))
-		            x_train = Matrix(scale_transform(x_scaler, Matrix(x[train, :]))')
-		        else
-		            x_train = Matrix(Matrix(x[train, :])')
-		        end
-		        y_train = Matrix(y[train]')
+					x_scaler = fit_scaler(scaler_x, Matrix(x[train, :]))
+					x_train = Matrix(scale_transform(x_scaler, Matrix(x[train, :]))')
+					x_valid = Matrix(scale_transform(x_scaler, Matrix(x[valid, :]))')
+				else
+					x_train = Matrix(Matrix(x[train, :])')
+					x_valid = Matrix(Matrix(x[valid, :])')
+				end
+				y_train = Matrix(y[train]')
+				y_valid = Matrix(y[valid]')
 				data = Flux.Data.DataLoader((x_train, y_train), shuffle = true, batchsize = nobs_per_batch)
-		    	j = 1
-		        while j < (n_epochs + 1)
-		            my_custom_train!(flux_model, loss, loss_init, data, optimizer)
-		            train_loss = loss(flux_model, loss_init, x_train, y_train)
-		            # println("epoch = " * string(j) * " training_loss = " * string(train_loss))
-		            j += 1
-		        end
+				j = 1
+				while j < (1 + n_epochs)
+					my_custom_train!(flux_model, loss, loss_init, data, optimizer)
+					valid_loss = loss(flux_model, loss_init, x_valid, y_valid)
+					push!(valid_loss_record, valid_loss)
+					push!(model_dict, Symbol("model" * string(j)) => flux_model)
+					if isnan(valid_loss) == false
+						if pullback == true
+							if j > 1
+								if valid_loss .>= valid_loss_record[j - 1]
+									early_stop_flag += 1
+								else
+									early_stop_flag -= 1
+								end
+								early_stop_flag = max(0, min(lcheck, early_stop_flag))
+								if early_stop_flag == lcheck
+									try
+										Flux.stop()
+									catch
+									finally
+									end
+									break
+								end
+							end
+						end
+					else
+						try
+							Flux.skip()
+						catch
+						finally
+						end
+					end
+					j += 1
+				end
+				if early_stop_flag == lcheck
+					model = model_dict[Symbol("model" * string(j - early_stop_flag))]
+				else
+					model = model_dict[Symbol("model" * string(n_epochs))]
+				end
 				println("train_iter = " * string(k) * "; epochs = " * string(j))
-		        flux_model_pred = flux_model_builder
-		        Flux.loadmodel!(flux_model_pred, flux_model);
-		        y_pred_train = flux_model_pred(x_train)
+				flux_model_pred = flux_model_builder
+				Flux.loadmodel!(flux_model_pred, model);
+				y_pred_valid = flux_model_pred(x_valid)
+				y_pred_train = flux_model_pred(x_train)
+				append!(x_meta, x[train, :])
+				append!(x_meta, x[valid, :])
 				append!(y_meta, y_pred_train)
+				append!(y_meta, y_pred_valid)
 				r2_train = round((Statistics.cor(y_train[1, :], y_pred_train[1, :]))^2, digits = r_squared_precision)
-		        rmse_train = round(sqrt(Flux.Losses.mse(y_pred_train, y_train)), digits = rmse_precision)
-				model_perform = [k r2_train rmse_train]
-		        if k == 1
-		            CSV.write(save_trained_model_at * "/stacking_model_training_records.csv", DataFrame(model_perform, [:train_iter, :r_squared_train, :rmse_train]))
-		        else
-		            CSV.write(save_trained_model_at * "/stacking_model_training_records.csv", DataFrame(model_perform, [:train_iter, :r_squared_train, :rmse_train]), append = true)
+	        	rmse_train = round(sqrt(Flux.Losses.mse(y_pred_train, y_train)), digits = rmse_precision)
+				r2_valid = round((Statistics.cor(y_valid[1, :], y_pred_valid[1, :]))^2, digits = r_squared_precision)
+	        	rmse_valid = round(sqrt(Flux.Losses.mse(y_pred_valid, y_valid)), digits = rmse_precision)
+				model_perform = [k r2_train r2_valid rmse_train rmse_valid]
+	        	if k == 1
+		            CSV.write(save_trained_model_at * "/stacking_model_training_records.csv", DataFrame(model_perform, [:train_iter, :r_squared_train, :r_squared_valid, :rmse_train, :rmse_valid]))
+	        	else
+		            CSV.write(save_trained_model_at * "/stacking_model_training_records.csv", DataFrame(model_perform, [:train_iter, :r_squared_train, :r_squared_valid, :rmse_train, :rmse_valid]), append = true)
 		        end
 			end
 		end
 		fetch(t)
 	end
 	# train the meta-learner
+	cv_train = cross_validate(Holdout_(meta_train_frac, false, 1), eachindex(y_meta))
 	flux_model = flux_model_builder
-	if isnothing(scaler_x) == false
-		x_scaler = fit_scaler(scaler_x, Matrix(x_meta))
-		if save_trained_model
-			BSON.@save(save_trained_model_at * "/saved_ensembled_meta_Xscaler(s)/Xscaler.bson", x_scaler)
-		end
-		x_train = Matrix(scale_transform(x_scaler, Matrix(x_meta))')
-	else
-		x_train = Matrix(Matrix(x_meta)')
+	train, valid = cv_train[1, ]
+	if pullback == true
+		valid_loss_record = []
+		early_stop_flag = 0
+		model_dict = Dict()
 	end
-	y_train = Matrix(y_meta')
+	if isnothing(scaler_x) == false
+		x_scaler = fit_scaler(scaler_x, Matrix(x_meta[train, :]))
+		x_train = Matrix(scale_transform(x_scaler, Matrix(x_meta[train, :]))')
+		x_valid = Matrix(scale_transform(x_scaler, Matrix(x_meta[valid, :]))')
+	else
+		x_train = Matrix(Matrix(x_meta[train, :])')
+		x_valid = Matrix(Matrix(x_meta[valid, :])')
+	end
+	y_train = Matrix(y_meta[train]')
+	y_valid = Matrix(y_meta[valid]')
 	data = Flux.Data.DataLoader((x_train, y_train), shuffle = true, batchsize = nobs_per_batch)
 	j = 1
-	while j < (n_epochs + 1)
+	while j < (1 + n_epochs)
 		my_custom_train!(flux_model, loss, loss_init, data, optimizer)
-		train_loss = loss(flux_model, loss_init, x_train, y_train)
-		println("epoch = " * string(j) * " training_loss = " * string(train_loss))
+		valid_loss = loss(flux_model, loss_init, x_valid, y_valid)
+		push!(valid_loss_record, valid_loss)
+		push!(model_dict, Symbol("model" * string(j)) => flux_model)
+		if isnan(valid_loss) == false
+			println("epoch = " * string(j) * " validation_loss = " * string(valid_loss))
+			if pullback == true
+				if j > 1
+					if valid_loss .>= valid_loss_record[j - 1]
+						early_stop_flag += 1
+					else
+						early_stop_flag -= 1
+					end
+					early_stop_flag = max(0, min(lcheck, early_stop_flag))
+					if early_stop_flag == lcheck
+						try
+							Flux.stop()
+						catch
+						finally
+						end
+						break
+					end
+				end
+			end
+		else
+			try
+				Flux.skip()
+			catch
+			finally
+			end
+		end
 		j += 1
 	end
+	if early_stop_flag == lcheck
+		model = model_dict[Symbol("model" * string(j - early_stop_flag))]
+	else
+		model = model_dict[Symbol("model" * string(n_epochs))]
+	end
 	flux_model_pred = flux_model_builder
-	Flux.loadmodel!(flux_model_pred, flux_model);
+	Flux.loadmodel!(flux_model_pred, model);
+	y_pred_valid = flux_model_pred(x_valid)
 	y_pred_train = flux_model_pred(x_train)
 	r2_train = round((Statistics.cor(y_train[1, :], y_pred_train[1, :]))^2, digits = r_squared_precision)
 	rmse_train = round(sqrt(Flux.Losses.mse(y_pred_train, y_train)), digits = rmse_precision)
-	model_perform = [r2_train rmse_train]
-	CSV.write(save_trained_model_at * "/ensembled_meta_model_training_records.csv", DataFrame(model_perform, [:r_squared_train, :rmse_train]))
+	r2_valid = round((Statistics.cor(y_valid[1, :], y_pred_valid[1, :]))^2, digits = r_squared_precision)
+	rmse_valid = round(sqrt(Flux.Losses.mse(y_pred_valid, y_valid)), digits = rmse_precision)
+	model_perform = [r2_train r2_valid rmse_train rmse_valid]
+	CSV.write(save_trained_model_at * "/ensembled_meta_model_training_records.csv", DataFrame(model_perform, [:r_squared_train, :r_squared_valid, :rmse_train, :rmse_valid]))
 	if save_trained_model
 		BSON.@save(save_trained_model_at * "/saved_ensembled_meta_model(s)/trained_model_.bson", flux_model)
 	end
